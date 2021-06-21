30c61471d11e2ea926dc59f6dc77b12d9a676371
Sockthing <sockthinggaming@gmail.com>
2021-06-21 22:04:59 +0200

    Added Potion Effects to Glow Ink Sac

--- src/minecraft/net/minecraft/src/SocksMobsItemGlowInk.java
+++ src/minecraft/net/minecraft/src/SocksMobsItemGlowInk.java
@@ -1,12 +1,18 @@
 package net.minecraft.src;
 
-public class SocksMobsItemGlowInk extends Item
-{
-    public SocksMobsItemGlowInk(int par1)
-    {
-        super(par1);
-        this.setCreativeTab(CreativeTabs.tabMaterials);
-        this.setUnlocalizedName("SocksMobsItemGlowInk");
-    }
-    
+public class SocksMobsItemGlowInk extends FCItemFood {
+	public SocksMobsItemGlowInk(int par1) {
+		super(par1, 1, 0.0F, false, "SocksMobsItemGlowInk");
+		this.setCreativeTab(CreativeTabs.tabMaterials);
+		this.setPotionEffect(Potion.nightVision.id, 5, 0,1.0F);
+		
+	}
+	
+	 public ItemStack onEaten(ItemStack var1, World var2, EntityPlayer var3)
+	    {
+		 	super.onEaten(var1, var2, var3);
+		 	var3.addPotionEffect(new PotionEffect(Potion.hunger.id, 5*20, 0));
+	        
+			return var1;
+	    }
 }
