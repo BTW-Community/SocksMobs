226467a7870c4eaf665914afa81706c1c58e0dd2
Sockthing <25046819+socklessthing@users.noreply.github.com>
2025-04-21 22:12:52 +0200

    Added Pig Textures
    - Famished
    - Starving
    - Breeding Harness

--- src/main/java/btw/community/sockthing/socksmobs/mixins/RenderPigMixin.java
+++ src/main/java/btw/community/sockthing/socksmobs/mixins/RenderPigMixin.java
@@ -17,12 +17,14 @@ public abstract class RenderPigMixin {
     private static final ResourceLocation PIG_DEFAULT_TEXTURE = new ResourceLocation("textures/entity/pig/pig.png");
 
     private static final ResourceLocation PIG_WARM_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/warm_pig.png");
-    private static final ResourceLocation PIG_WARM_FAMISHED_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/warm_pig.png");
-    private static final ResourceLocation PIG_WARM_STARVING_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/warm_pig.png");
+    private static final ResourceLocation PIG_WARM_FAMISHED_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/warm_pig_famished.png");
+    private static final ResourceLocation PIG_WARM_STARVING_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/warm_pig_starving.png");
+    private static final ResourceLocation PIG_WARM_HARNESS_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/warm_pig_harness.png");
 
     private static final ResourceLocation PIG_COLD_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/cold_pig.png");
-    private static final ResourceLocation PIG_COLD_FAMISHED_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/cold_pig.png");
-    private static final ResourceLocation PIG_COLD_STARVING_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/cold_pig.png");
+    private static final ResourceLocation PIG_COLD_FAMISHED_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/cold_pig_famished.png");
+    private static final ResourceLocation PIG_COLD_STARVING_TEXTURE = new ResourceLocation("socksmobs:textures/entity/pig/cold_pig_starving.png");
+    private static final ResourceLocation PIG_COLD_HARNESS_TEXTURE = new ResourceLocation("btw:textures/entity/mob/pig/cold_pig_harness.png");
 
     public RenderPigMixin(ModelBase model, float par2) {
         super();
@@ -57,14 +59,14 @@ public abstract class RenderPigMixin {
     }
 
     private ResourceLocation getPigFamishedTexture(int pigType) {
-        if (pigType == 1) return PIG_COLD_STARVING_TEXTURE;
-        if (pigType == 2) return PIG_WARM_STARVING_TEXTURE;
+        if (pigType == 1) return PIG_COLD_FAMISHED_TEXTURE;
+        if (pigType == 2) return PIG_WARM_FAMISHED_TEXTURE;
         return PIG_FAMISHED_TEXTURE;
     }
 
     private ResourceLocation getPigHarnessTexture(int pigType){
-        if (pigType == 1) return PIG_COLD_STARVING_TEXTURE;
-        if (pigType == 2) return PIG_WARM_STARVING_TEXTURE;
+        if (pigType == 1) return PIG_COLD_HARNESS_TEXTURE;
+        if (pigType == 2) return PIG_WARM_HARNESS_TEXTURE;
         return MR_PIG_TEXTURE;
     }
 }
