94e8cb5e7faf00219566491a9a0a412d9bf7fc64
Sockthing <25046819+socklessthing@users.noreply.github.com>
2025-06-28 02:51:56 +0200

    Add dark subtype textures for chickens
    Introduces new texture resources and selection logic for a 'dark' chicken subtype, including baby chicks, chickens, and roosters in normal, famished, and starving states. Also adds the DARK subtype constant to ChickenUtils.

--- src/main/java/btw/community/sockthing/socksmobs/utils/ChickenTextures.java
+++ src/main/java/btw/community/sockthing/socksmobs/utils/ChickenTextures.java
@@ -11,13 +11,25 @@ public class ChickenTextures {
     public static final ResourceLocation BABY_CHICK_FAMISHED_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/baby_chick_famished.png");
     public static final ResourceLocation BABY_CHICK_STARVING_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/baby_chick_starving.png");
 
+    private static final ResourceLocation BABY_CHICK_DARK_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/baby_chick_dark.png");
+    private static final ResourceLocation BABY_CHICK_DARK_FAMISHED_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/baby_chick_dark_famished.png");
+    private static final ResourceLocation BABY_CHICK_DARK_STARVING_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/baby_chick_dark_starving.png");
+
     public static final ResourceLocation CHICKEN_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/chicken.png");
     public static final ResourceLocation CHICKEN_FAMISHED_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/chicken_famished.png");
     public static final ResourceLocation CHICKEN_STARVING_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/chicken_starving.png");
 
+    private static final ResourceLocation CHICKEN_DARK_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/chicken_dark.png");
+    private static final ResourceLocation CHICKEN_DARK_FAMISHED_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/chicken_dark_famished.png");
+    private static final ResourceLocation CHICKEN_DARK_STARVING_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/chicken_dark_starving.png");
+
     public static final ResourceLocation ROOSTER_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/rooster.png");
     public static final ResourceLocation ROOSTER_FAMISHED_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/rooster_famished.png");
     public static final ResourceLocation ROOSTER_STARVING_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/rooster_starving.png");
+
+    private static final ResourceLocation ROOSTER_DARK_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/rooster_dark.png");
+    private static final ResourceLocation ROOSTER_DARK_FAMISHED_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/rooster_dark_famished.png");
+    private static final ResourceLocation ROOSTER_DARK_STARVING_TEXTURE = new ResourceLocation("socksmobs:textures/entity/chicken/rooster_dark_starving.png");
 
     public static ResourceLocation getBabyChickTexture(EntityChicken chicken){
         int subtype = ((EntityAnimalInterface) chicken).getType();
@@ -27,6 +39,11 @@ public class ChickenTextures {
             if (hungerLevel == MobUtils.NORMAL) return BABY_CHICK_TEXTURE;
             else if (hungerLevel == MobUtils.FAMISHED) return BABY_CHICK_FAMISHED_TEXTURE;
             else if (hungerLevel == MobUtils.STARVING) return BABY_CHICK_STARVING_TEXTURE;
+        }
+        else if (subtype == ChickenUtils.DARK){
+            if (hungerLevel == MobUtils.NORMAL) return BABY_CHICK_DARK_TEXTURE;
+            else if (hungerLevel == MobUtils.FAMISHED) return BABY_CHICK_DARK_FAMISHED_TEXTURE;
+            else if (hungerLevel == MobUtils.STARVING) return BABY_CHICK_DARK_STARVING_TEXTURE;
         }
 
         return MobUtils.NULL_TEXTURE;
@@ -41,6 +58,11 @@ public class ChickenTextures {
             else if (hungerLevel == MobUtils.FAMISHED) return CHICKEN_FAMISHED_TEXTURE;
             else if (hungerLevel == MobUtils.STARVING) return CHICKEN_STARVING_TEXTURE;
         }
+        else if (subtype == ChickenUtils.DARK){
+            if (hungerLevel == MobUtils.NORMAL) return CHICKEN_DARK_TEXTURE;
+            else if (hungerLevel == MobUtils.FAMISHED) return CHICKEN_DARK_FAMISHED_TEXTURE;
+            else if (hungerLevel == MobUtils.STARVING) return CHICKEN_DARK_STARVING_TEXTURE;
+        }
 
         return MobUtils.NULL_TEXTURE;
     }
@@ -53,6 +75,11 @@ public class ChickenTextures {
             if (hungerLevel == MobUtils.NORMAL) return ROOSTER_TEXTURE;
             else if (hungerLevel == MobUtils.FAMISHED) return ROOSTER_FAMISHED_TEXTURE;
             else if (hungerLevel == MobUtils.STARVING) return ROOSTER_STARVING_TEXTURE;
+        }
+        else if (subtype == ChickenUtils.DARK){
+            if (hungerLevel == MobUtils.NORMAL) return ROOSTER_DARK_TEXTURE;
+            else if (hungerLevel == MobUtils.FAMISHED) return ROOSTER_DARK_FAMISHED_TEXTURE;
+            else if (hungerLevel == MobUtils.STARVING) return ROOSTER_DARK_STARVING_TEXTURE;
         }
 
         return MobUtils.NULL_TEXTURE;
--- src/main/java/btw/community/sockthing/socksmobs/utils/ChickenUtils.java
+++ src/main/java/btw/community/sockthing/socksmobs/utils/ChickenUtils.java
@@ -3,4 +3,5 @@ package btw.community.sockthing.socksmobs.utils;
 public class ChickenUtils extends MobUtils {
     //SUBTYPES
     public static final int DEFAULT = 0;
+    public static final int DARK = 1;
 }
