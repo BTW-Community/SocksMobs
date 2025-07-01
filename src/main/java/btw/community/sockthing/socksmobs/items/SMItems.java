56aab266c475babccbd7099ad0f1451224561d89
Sockthing <sockthinggaming@gmail.com>
2025-07-01 21:11:54 +0200

    Update unfertilized egg item
    Refactors UnfertilizedEggItem to set unlocalized and texture names internally, and updates its localization key.

--- src/main/java/btw/community/sockthing/socksmobs/items/SMItems.java
+++ src/main/java/btw/community/sockthing/socksmobs/items/SMItems.java
@@ -9,6 +9,21 @@ public class SMItems {
     public static int UNFERTILIZED_EGG_ID = 3999; //TODO: CHECK ID's
 
     public static void initItems(){
-        unfertilizedEgg = new UnfertilizedEggItem(UNFERTILIZED_EGG_ID - 256, "socksmobs:unfertilized_egg");
+        unfertilizedEgg = new UnfertilizedEggItem(UNFERTILIZED_EGG_ID - 256);
+
+        wolfArmorLeather = new WolfArmorItem(WOLF_ARMOR_LEATHER_ID - 256, EnumArmorMaterial.CLOTH, WolfArmorItem.LEATHER,
+                1, 0, 0, 10, 2);
+
+        wolfArmorIron = new WolfArmorItem(WOLF_ARMOR_IRON_ID - 256, EnumArmorMaterial.IRON, WolfArmorItem.IRON,
+                1, 0, 0f, 25, 2);
+
+        wolfArmorGold = new WolfArmorItem(WOLF_ARMOR_GOLD_ID - 256, EnumArmorMaterial.GOLD, WolfArmorItem.GOLD,
+                1, 0, 0f, 30, 3);
+
+        wolfArmorDiamond = new WolfArmorItem(WOLF_ARMOR_DIAMOND_ID - 256, EnumArmorMaterial.DIAMOND, WolfArmorItem.DIAMOND,
+                1, 0, 0f, 30, 2);
+
+        wolfArmorSteel = new WolfArmorItem(WOLF_ARMOR_STEEL_ID - 256, EnumArmorMaterial.DIAMOND, WolfArmorItem.STEEL,
+                1, 0, 0.1f, 30, 4);
     }
 }
